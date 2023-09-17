b91b00799c5dd41804af7f53a57d2893e754b776
mahmoudzahran <mah.zahran777@gmaol.com>
2023-09-17 22:47:52 +0300

    initial commit

--- app/src/main/java/com/zahran/movieapp/data/source/remote/Resource.kt
+++ app/src/main/java/com/zahran/movieapp/data/source/remote/Resource.kt
@@ -2,6 +2,8 @@ package com.zahran.movieapp.data.source.remote
 
 sealed class Resource<T>(val data: T? = null, val message: String? = null) {
     class Loading<T>(data: T? = null) : Resource<T>(data)
+    class Loading1<T>(data: T? = null) : Resource<T>(data)
     class Success<T>(data: T?) : Resource<T>(data)
+    class Successful<T>(data: T?) : Resource<T>(data)
     class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
 }
